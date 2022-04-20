fea2f05ac4a094e3af888050b261278c0d2f4422
jack <1324442890@qq.com>
2022-04-20 14:11:05 +0800

    finally commit

--- src/main/java/com/wqbr/clouddemo/App.java
+++ src/main/java/com/wqbr/clouddemo/App.java
@@ -9,9 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("master add");
-        System.out.println("hot-fix add");
-        System.out.println("merge ok");
-        System.out.println("pull test");
     }
 }
